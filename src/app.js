6b7061bf64776c5cdd87633440f988553223e724
Sahadat-Hossen1 <sahadat.hossen1435@gmail.com>
2026-05-02 08:52:18 +0600

    install and use cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express"
 import dotenv from "dotenv";
+import cors from "cors"
 import todoRouter from "./routers/todo.router.js";
+
 dotenv.config()
 const app=express()
+app.use(cors())
 app.use(express.json())
 app.get("/",(req,res)=>{
     res.status(200).send("todos server working ")
